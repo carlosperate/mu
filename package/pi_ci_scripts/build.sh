4c986339296da938bef9536f7dc1b55f65f7b8a6
carlosperate <carlosperate@users.noreply.github.com>
2016-12-17 15:51:43 +0000

    Update Pi build script with deb file creation

--- package/pi_ci_scripts/build.sh
+++ package/pi_ci_scripts/build.sh
@@ -12,7 +12,11 @@ if [ -d "$DEB_BUILD_DIR" ]; then
     rm -r "$DEB_BUILD_DIR" 
 fi
 
-git-buildpackage --git-upstream-tree="$GIT_COMMIT" --git-ignore-branch --git-export-dir="$DEB_BUILD_DIR" --git-force-create --git-purge  -uc -us
+PREV_COMMIT="`git rev-parse HEAD^1`"
+git-dch --ignore-branch --since="$PREV_COMMIT" --snapshot
+
+git-buildpackage --git-upstream-tree="$GIT_COMMIT" --git-ignore-branch --git-ignore-new --git-export-dir="$DEB_BUILD_DIR" --git-force-create  -uc -us
+#git-buildpackage --git-upstream-tree="$GIT_COMMIT" --git-ignore-branch  --git-export-dir="$DEB_BUILD_DIR" --git-force-create  -uc -us
 
 for f in "$DEB_BUILD_DIR/"*; do
     if [ ${f: -4} == ".deb" ]; then
@@ -27,8 +31,7 @@ done
 python3 ../pyinstaller/pyinstaller.py package/pyinstaller.spec
 du -sk dist/
 if [ -f dist/mu ]; then
-    exec_name="mu-$DATE_"
+    exec_name="mu-$DATE_.bin"
     mv "dist/mu" "dist/$exec_name"
     echo "EXECUTABLE_NAME=$exec_name"
 fi
-
